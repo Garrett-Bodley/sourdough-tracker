91fc4e3631eb5bbd19fc4d348a9e95e7e5bbe0f5
Garrett <Garrett.Bodley@gmail.com>
2020-12-07 15:28:04 -0500

    associates user with recipes and ingredients

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :users_recipes
+  has_many :recipes, through: :users_recipes
+  has_many :ingredients, through: :recipes
+  has_secure_password
+  validates :username, :password, presence: :true
 end
