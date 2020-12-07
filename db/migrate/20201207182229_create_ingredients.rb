0c22a4057fca658d1caf54dc614f5e4560bb8773
Garrett <Garrett.Bodley@gmail.com>
2020-12-07 13:57:17 -0500

    make ingredient model

--- db/migrate/20201207182229_create_ingredients.rb
+++ db/migrate/20201207182229_create_ingredients.rb
@@ -1,7 +1,8 @@
 class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
-
+      t.string :name
+      t.string :description
       t.timestamps null: false
     end
   end
