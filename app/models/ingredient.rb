0a8fa7f4d904863d74e269de8245f97dc15a112d
Garrett <Garrett.Bodley@gmail.com>
2020-12-09 14:48:00 -0500

    Add bakes and baker_percentage relationship

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,7 @@ class Ingredient < ActiveRecord::Base
   include Concerns::Slugify
   extend Concerns::FindBySlug
   has_many :recipes_ingredients
+  has_many :baker_percentages
   has_many :recipes, through: :recipes_ingredients
   belongs_to :user
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ActiveRecord::Base
   include Concerns::Slugify
   extend Concerns::FindBySlug
   has_many :recipes_ingredients
+  has_many :baker_percentages
+  has_many :bakes
   has_many :ingredients, through: :recipes_ingredients
   belongs_to :user
 end
