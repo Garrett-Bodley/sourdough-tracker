1fab9a54d98071d896e1694e886508428e71f52f
Garrett <Garrett.Bodley@gmail.com>
2020-12-09 21:26:42 -0500

    Create new class method to update or create instance

--- app/models/baker_percentage.rb
+++ app/models/baker_percentage.rb
@@ -1,4 +1,11 @@
 class BakerPercentage < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
+
+  def self.update_or_create(hash)
+    unless found = self.find_by(ingredient_id: hash[:ingredient_id], recipe_id: hash[:recipe_id])
+      self.create(hash)
+    end
+    found.update(hash)
+  end
 end
