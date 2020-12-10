d994775421251c78c6bfce92ea2fccd4997468d0
Garrett <Garrett.Bodley@gmail.com>
2020-12-10 13:17:35 -0500

    Debug belongs_to relationship

--- app/models/baker_percentage.rb
+++ app/models/baker_percentage.rb
@@ -1,7 +1,7 @@
 class BakerPercentage < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
-  belongs_to :user_id
+  belongs_to :user
 
   def self.update_or_create(hash)
     unless found = self.find_by(ingredient_id: hash[:ingredient_id], recipe_id: hash[:recipe_id], user_id: hash[:user_id])
