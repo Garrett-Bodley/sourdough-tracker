3899392d9508937f9b260ecdfc1d1c67e04217b7
Garrett <Garrett.Bodley@gmail.com>
2020-12-12 22:12:59 -0500

    Change find_by slug to also use user_id. Sanitize slug output.

--- app/models/concerns.rb
+++ app/models/concerns.rb
@@ -1,12 +1,12 @@
 module Concerns
   module FindBySlug
-    def find_by_slug(slug)
-      self.all.find{|item| item.slug == slug}
+    def find_by_slug_and_user_id(slug:, user_id:)
+      self.all.find{|item| item.slug == slug && item.user_id == user_id}
     end
   end
   module Slugify
     def slug
-      self.name.downcase.split.join("-")
+      self.name.downcase.gsub(/[~`!@#$%^&*()+=|}{\[\]'";:><,.\/]/, "").split.join("-")
     end
   end
 end
