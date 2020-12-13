cfd123af4d18130581628c7971dacde74dbd09bc
Garrett <Garrett.Bodley@gmail.com>
2020-12-12 23:24:12 -0500

    Destroy recipes_ingredients and baker_percentages when ingredient or recipe is destroyed.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,8 @@
 class Ingredient < ActiveRecord::Base
   include Concerns::Slugify
   extend Concerns::FindBySlug
-  has_many :recipes_ingredients
-  has_many :baker_percentages
+  has_many :recipes_ingredients, dependent: :destroy
+  has_many :baker_percentages, dependent: :destroy
   has_many :recipes, through: :recipes_ingredients
   belongs_to :user
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ActiveRecord::Base
   include Concerns::Slugify
   extend Concerns::FindBySlug
-  has_many :recipes_ingredients
-  has_many :baker_percentages
+  has_many :recipes_ingredients, dependent: :destroy
+  has_many :baker_percentages, dependent: :destroy
   has_many :bakes
   has_many :ingredients, through: :recipes_ingredients
   belongs_to :user
